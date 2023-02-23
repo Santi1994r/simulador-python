1570fa2089ffed1b2d9cee141c320c3d4b6d30a3
Santi1994r <santiagoruiz9416@gmail.com>
2023-02-22 23:00:41 -0300

    add register function

---
+++ simulator.py
@@ -0,0 +1,19 @@
+import json
+
+DB = {}
+
+def register(DB):
+    name = input('Ingrese su nombre').lower()
+    password = input(
+        'Ingrese su password. Minimo 8 caracteres y maximo 15.').lower()
+
+    while (not name or name.isdigit() or len(name) < 3 or len(name) > 12 or not password or len(password) < 8 or len(password) > 15):
+        name = input('Ingrese su nombre').lower()
+        password = input('Ingrese su password. Minimo 8 caracteres y maximo 15.').lower()
+
+    DB["name"] = name
+    DB["password"] = password
+    
+    return DB
+
+print(register(DB))
